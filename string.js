5d4a0acdc552dea3a2ff5bcd1417583f1539806e
revanth045 <revanthchandaka2002@gmail.com>
2023-09-10 10:46:26 +0530

    10/09/23

--- string.js
+++ string.js
@@ -1,4 +1,9 @@
-let a="revanth"
+/*let a="revanth"
 console.log(a[6])
 //template literal
 console.log(`${a} is 22 years old.`)//this is called as a string interpolation
+console.log("Hello \'world")//escape character(\)*/
+//string method
+let a="revanth is 22 years old"
+let b="years"
+console.log(`print ${b} if it is present in ${a}=> ${a.includes(b)?"yes,present":"no"}`)
---
+++ stringmethods.js
@@ -0,0 +1,3 @@
+let name='REVAnth'
+console.log(name.toLowerCase())//string to lowercase    
+console.log(name.toUpperCase())//string to uppercase
